9efc4ec3058e29ab1899d0616e42146f433ebaeb
范晓豪 <fanxiaohaohaha@qq.com>
2023-06-07 08:57:34 +0800

    fix：优化删除，增加脚本

--- Server/bulkDeletion.js
+++ Server/bulkDeletion.js
@@ -1,62 +1,54 @@
 const {getTheList, deleteRow} = require('../api/xiaoShuo')
 const colors = require('colors-console')
 
-init(101)
-
-// 批量删除行记录
 async function init (num) {
-  let total = Math.ceil(num / 100) || 0
-  let counter = 0
+  const pageSize = 100
+  const total = Math.ceil(num / pageSize)
+  const delay = 3000
 
-  // 定义递归函数
-  function bulkTasks () {
-    deleteTheRowRecord()
-    counter++
+  console.log(colors('green', `共${total}次，每次间隔3秒`))
 
-    // 判断是否达到执行次数上限
-    if (counter < total) {
-      setTimeout(bulkTasks, 3000)
-    } else {
-      console.log(colors('green', `共${total}次，每次间隔3秒`))
+  for (let pageIndex = 1; pageIndex <= total; pageIndex++) {
+    try {
+      const allData = await getList(pageIndex, pageSize)
+      const rowIds = allData.map(obj => obj.rowid)
+      const joinedRowIds = rowIds.join(',')
+
+      await deleteTheRow(joinedRowIds)
+
+      await sleep(delay)
+    } catch (error) {
+      console.error('Failed to process batch:', error)
     }
   }
-
-  // 开始执行递归打印
-  bulkTasks()
 }
 
-async function deleteTheRowRecord () {
-  let allData = await getList()
-  let join = allData.join(',')
-  deleteTheRow(join)
-}
-
-// 获取列表 POST
-async function getList () {
+async function getList (pageIndex, pageSize) {
   try {
     const data = {
       worksheetId: '647321200279bf9fdb479428',
       viewId: '646db2c44b798e85aaf41d77',
-      pageSize: 100,
-      pageIndex: 1,
+      pageSize,
+      pageIndex,
     }
     const listData = await getTheList(data)
-    return listData.data.rows.map(obj => obj.rowid)
+    return listData.data.rows
   } catch (error) {
     console.error('Failed to get list data:', error)
+    return []
   }
 }
 
-// 删除行记录 POST
 async function deleteTheRow (allRowId) {
   try {
-    const data = {
-      worksheetId: '647321200279bf9fdb479428',
-      rowId: allRowId || '',
-      triggerWorkflow: false,
-    }
-    await deleteRow(data)
+    await deleteRow({rowId: allRowId || ''})
   } catch (error) {
     console.error('Failed to delete row:', error)
   }
 }
+
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+init(101)
--- api/xiaoShuo.js
+++ api/xiaoShuo.js
@@ -1,8 +1,8 @@
-const { AppKey, Sign, WorksheetId } = require('../config')
+const {AppKey, Sign, WorksheetId} = require('../config')
 const http = require('./http')
 
 // 获取列表 POST
-function getTheList(val) {
+function getTheList (val) {
   return http({
     url: 'getFilterRows',
     method: 'post',
@@ -15,20 +15,22 @@ function getTheList(val) {
 }
 
 // 删除行记录 POST
-function deleteRow(val) {
+function deleteRow (val) {
   return http({
     url: 'deleteRow',
     method: 'post',
     data: {
       appKey: AppKey,
       sign: Sign,
+      worksheetId: WorksheetId,
+      triggerWorkflow: false,
       ...val
     }
   })
 }
 
 // 批量新建行记录 POST
-function bulkRowRecords(data) {
+function bulkRowRecords (data) {
   return http({
     url: 'addRows',
     method: 'post',
