96e0cd9d0fd3c8f04d573d433d67b6a873c75bf3
范晓豪 <fanxiaohaohaha@qq.com>
2023-06-07 12:11:24 +0800

    优化输入

--- Server/bulkUploads.js
+++ Server/bulkUploads.js
@@ -6,15 +6,16 @@ const {print} = require('../utils')
 const colors = require('colors-console')
 const readline = require('readline')
 
+let customizationFile = ''
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-// 提示用户输入命令
-rl.question('是否进行批量上传（y/n）：', async (answer) => {
+rl.question('请输入同级文件夹名:', async (answer) => {
   // 根据用户输入的命令进行下一步操作
-  if (answer === 'y') {
+  if (answer) {
+    customizationFile = answer
     console.log(colors('green', '开始批量上传'))
     await uploadTheFiles()
   } else {
@@ -24,15 +25,16 @@ rl.question('是否进行批量上传（y/n）：', async (answer) => {
   rl.close()
 })
 
-async function uploadTheFiles() {
+
+async function uploadTheFiles () {
   // 获取文件夹内的文件名和对应的 Base64 值
-  const folderPath = path.resolve(__dirname, '../../4')
+  const folderPath = path.resolve(__dirname, `../../${customizationFile}`)
   const filesData = getFilesData(folderPath)
 
   // 根据总数上传文件
   const total = filesData.length
 
-  for (let i = 0; i < total; i ++) {
+  for (let i = 0; i < total; i++) {
     const fileData = filesData[i]
     // 逐个上传文件
     try {
@@ -46,11 +48,11 @@ async function uploadTheFiles() {
   console.log(colors('bright', `上传完成，共上传 ${total} 个文件`))
 }
 
-async function bulkUploads(filesData) {
+async function bulkUploads (filesData) {
   return await bulkRowRecords(filesData)
 }
 
-function getFilesData(folderPath) {
+function getFilesData (folderPath) {
   try {
     const fileNames = fs.readdirSync(folderPath)
     return fileNames.map((fileName) => {
